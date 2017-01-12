16b6e8091d162e0d7deb16185fe947257ee63b27
NathanHealea <nate.healea@gmail.com>
2017-01-11 18:40:52 -0800

    rebasing project, going a new direction

--- app/README.md
+++
@@ -1,3 +0,0 @@
-# Valley Library Interactive Map
-
-**[Progress Blog](https://github.com/NathanHealea/ValleyLibraryInteractiveMap/wiki/)**
--- app/server_script.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-
-
-data=`cat data.txt`
-curl -H "Content-Type: application/json" -X POST -d '{"value":"'$data'"}' http://localhost:3000/items
-
