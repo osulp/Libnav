edcbcd0f03ff715dd3a8612e4de16fc7666eb680
NathanHealea <nate.healea@gmail.com>
2017-01-17 22:29:47 -0800

    added redirect for unauthorized user trying to access dashboard

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -1,9 +1,20 @@
 var express = require('express');
 var router = express.Router();
 
+
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('dashboard/index', { title: 'Express' });
+router.get('/', function (req, res, next) {
+    if (req.session.authenticated) {
+        res.render('dashboard/index', {title: 'Express'});
+    }
+    res.render('error/index', {
+        message: '<div class="fa-container">' +
+        '<h1>' +
+        '<i class="fa fa-exclamation-triangle fa-2x fa-vertical"></i> ' +
+        'Access Denied</h1>' +
+        '</div>',
+        body: '<div class="col-lg-12"><h3>Please login to continue.</h3></div>'
+    });
 });
 
 
