5939d88ab9ce21e263158e4543d3d77e3f8d4694
NathanHealea <nate.healea@gmail.com>
2017-01-19 23:41:03 -0800

    implimented login for user

--- routes/user.js
+++ routes/user.js
@@ -2,13 +2,20 @@ var express = require('express');
 var router = express.Router();
 
 /* GET users listing. */
-router.get('/', function(req, res, next) {
+/*router.get('/', function(req, res, next) {
   res.send('respond with a resource');
-});
+});*/
 
 /* GET user login */
 router.get('/login', function(req, res, next) {
-  res.send('respond with a resource');
+  res.render('user/login', {title: 'Express'});
+});
+
+/* POST user login */
+router.post('/login', function(req, res, next) {
+  console.log(req.body.username);
+  var body = '<p>Username: ' + req.body.username + '</p><p>Password: ' + req.body.password + '</p>';
+  res.render('error/development', {title: 'Checking User login POST Data', body: body });
 });
 
 module.exports = router;
