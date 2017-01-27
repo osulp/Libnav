e4f5acb1fa1837702ee704e0c74d3cb8cc1724e3
NathanHealea <nate.healea@gmail.com>
2017-01-26 18:32:44 -0800

    added routes for all forms.

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -37,7 +37,7 @@ router.get('/known', function (req, res, next) {
 /* Post Know Location page */
 router.post('/known', function (req, res, next) {
     if (req.session.isAuthenticated) {
-        res.render('dashboard/known', {title: 'Known Location Form'})
+        res.render('dashboard/known')
     }else {
         res.render('error/index', {
             message: '<div class="fa-container">' +
@@ -54,6 +54,86 @@ router.post('/known', function (req, res, next) {
 router.get('/unknown', function (req, res, next) {
     if (req.session.isAuthenticated) {
         res.render('dashboard/unknown')
+    }else {
+        res.render('error/index', {
+            message: '<div class="fa-container">' +
+            '<h1>' +
+            '<i class="fa fa-exclamation-triangle fa-2x fa-vertical"></i> ' +
+            'Access Denied</h1>' +
+            '</div>',
+            body: '<div class="col-lg-12"><h3>Please login to continue.</h3></div>'
+        });
+    }
+});
+
+/* Post Unknown Location page */
+router.post('/unknown', function (req, res, next) {
+    if (req.session.isAuthenticated) {
+        res.render('dashboard/known' )
+    }else {
+        res.render('error/index', {
+            message: '<div class="fa-container">' +
+            '<h1>' +
+            '<i class="fa fa-exclamation-triangle fa-2x fa-vertical"></i> ' +
+            'Access Denied</h1>' +
+            '</div>',
+            body: '<div class="col-lg-12"><h3>Please login to continue.</h3></div>'
+        });
+    }
+});
+
+/* Get Room Location page */
+router.get('/room', function (req, res, next) {
+    if (req.session.isAuthenticated) {
+        res.render('dashboard/room')
+    }else {
+        res.render('error/index', {
+            message: '<div class="fa-container">' +
+            '<h1>' +
+            '<i class="fa fa-exclamation-triangle fa-2x fa-vertical"></i> ' +
+            'Access Denied</h1>' +
+            '</div>',
+            body: '<div class="col-lg-12"><h3>Please login to continue.</h3></div>'
+        });
+    }
+});
+
+/* Post Room Location page */
+router.post('/room', function (req, res, next) {
+    if (req.session.isAuthenticated) {
+        res.render('dashboard/room' )
+    }else {
+        res.render('error/index', {
+            message: '<div class="fa-container">' +
+            '<h1>' +
+            '<i class="fa fa-exclamation-triangle fa-2x fa-vertical"></i> ' +
+            'Access Denied</h1>' +
+            '</div>',
+            body: '<div class="col-lg-12"><h3>Please login to continue.</h3></div>'
+        });
+    }
+});
+
+/* Get Service Point Location page */
+router.get('/room', function (req, res, next) {
+    if (req.session.isAuthenticated) {
+        res.render('dashboard/service')
+    }else {
+        res.render('error/index', {
+            message: '<div class="fa-container">' +
+            '<h1>' +
+            '<i class="fa fa-exclamation-triangle fa-2x fa-vertical"></i> ' +
+            'Access Denied</h1>' +
+            '</div>',
+            body: '<div class="col-lg-12"><h3>Please login to continue.</h3></div>'
+        });
+    }
+});
+
+/* Post Service Point Location page */
+router.post('/room', function (req, res, next) {
+    if (req.session.isAuthenticated) {
+        res.render('dashboard/service' )
     }else {
         res.render('error/index', {
             message: '<div class="fa-container">' +
