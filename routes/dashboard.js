96fd8440d4956f1c51bb6193c56a02d6ce2e6ce6
NathanHealea <nate.healea@gmail.com>
2017-01-26 18:28:48 -0800

    Unknown location created

--- routes/dashboard.js
+++ routes/dashboard.js
@@ -21,6 +21,22 @@ router.get('/', function (req, res, next) {
 /* Get Known Location page */
 router.get('/known', function (req, res, next) {
     if (req.session.isAuthenticated) {
+        res.render('dashboard/known')
+    }else {
+        res.render('error/index', {
+            message: '<div class="fa-container">' +
+            '<h1>' +
+            '<i class="fa fa-exclamation-triangle fa-2x fa-vertical"></i> ' +
+            'Access Denied</h1>' +
+            '</div>',
+            body: '<div class="col-lg-12"><h3>Please login to continue.</h3></div>'
+        });
+    }
+});
+
+/* Post Know Location page */
+router.post('/known', function (req, res, next) {
+    if (req.session.isAuthenticated) {
         res.render('dashboard/known', {title: 'Known Location Form'})
     }else {
         res.render('error/index', {
@@ -33,6 +49,23 @@ router.get('/known', function (req, res, next) {
         });
     }
 });
+
+/* Get Unknown Location page */
+router.get('/unknown', function (req, res, next) {
+    if (req.session.isAuthenticated) {
+        res.render('dashboard/unknown')
+    }else {
+        res.render('error/index', {
+            message: '<div class="fa-container">' +
+            '<h1>' +
+            '<i class="fa fa-exclamation-triangle fa-2x fa-vertical"></i> ' +
+            'Access Denied</h1>' +
+            '</div>',
+            body: '<div class="col-lg-12"><h3>Please login to continue.</h3></div>'
+        });
+    }
+});
+
 
 
 module.exports = router;
