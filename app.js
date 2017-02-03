a403a60c808a5748202d8c7a5734a18eb63b4636
NathanHealea <nate.healea@gmail.com>
2017-02-03 10:16:43 -0800

    cleaned up files

--- app.js
+++ app.js
@@ -7,10 +7,6 @@ var favicon = require('serve-favicon');
 var logger = require('morgan');
 var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
-var PF = require('pathfinding');
-var d3 = require('d3');
-var lodash = require('lodash');
-
 
 var config = null;
 
@@ -65,8 +61,12 @@ app.use('/d3', express.static(__dirname+ '/node_modules/d3'));
 app.use('/approvejs', express.static(__dirname + '/node_modules/approvejs/dist/'));
 //lodash
 app.use('/lodash', express.static(__dirname + '/node_modules/lodash/'));
+
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
+
+
 app.use(logger('dev'));
 app.use(bodyParser.json()); // support json encoded bodies
 app.use(bodyParser.urlencoded({extended: true})); // support encoded bodies
