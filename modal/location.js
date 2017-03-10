9cb3bafc68983fa8df59aa24556d5d439e8e3941
NathanHealea <nate.healea@gmail.com>
2017-03-09 18:48:10 -0800

    adding funcitonality to ther forms

--- modal/location.js
+++ modal/location.js
@@ -108,6 +108,25 @@ exports.getLocations = function(callback){
     db.connection.end();
 };
 
+/*exports.getLocations = function(id, callback){
+    // create database connection
+    db.createConnection();
+
+    // connect to database
+    db.connection.connect();
+
+    // insert attributes
+    db.connection.query('SELECT l.id, l.floor, l.name, l.type, l.url, l.room_number , l.room_cap , l.data_point from location l', function (error, results, fields) {
+        if (error) throw error;
+        console.log(results);
+        callback(results);
+    });
+
+    // close connection to database
+    db.connection.end();
+};*/
+
+
 exports.getAttributes = function ( location, callback){
     // create database connection
     db.createConnection();
