12fabec71b8fe4e7eee8fe7004f200b7fb1bb967
NathanHealea <nate.healea@gmail.com>
2017-03-31 10:26:47 -0700

    refactor backend

--- routes/user.js
+++ routes/user.js
@@ -1,18 +1,35 @@
 var express = require('express');
 var router = express.Router();
 var user = require('../classes/user');
-/* GET users listing. */
-/*router.get('/', function(req, res, next) {
- res.send('respond with a resource');
- });*/
+var CASAuthentication = require('cas-authentication');
+
+
+// Create a new instance of CASAuthentication. 
+var cas = new CASAuthentication({
+    cas_url     : 'https://login.oregonstate.edu/cas/login',
+    service_url : '10.214.154.52:3000/user/login/cas'
+});
+
 
 /* GET user login */
 router.get('/login', function (req, res, next) {
-    res.render('user/login');
+    res.redirect(cas.cas_url);
 });
 
 /* POST user login */
 router.post('/login', function (req, res, next) {
+    // get info back to CAS 
+
+});
+
+/* GET user login for master */
+router.get('/login/master', function (req, res, next) {
+    res.render('user/login');
+});
+
+/* POST user login for master */
+router.post('/login/master', function (req, res, next) {
+    console.log("In post method")
     var isAuthenticated = false;
 
     isAuthenticated = user.authenticate(req.body.username, req.body.password);
